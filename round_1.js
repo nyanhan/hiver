ac5c2b61595caecece873f48311366a0b0475513
han.yan <hany.oy@gmail.com>
2014-08-16 13:59:08 +0800

    fix bug

--- round_1.js
+++ round_1.js
@@ -6,11 +6,12 @@ console.log("Input: " + input);
 var stick_len = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
 
 // A + B = C => (0 <= C.length - A.length <= 1)
-// max = (Input - 4) / 2
+// B 至少 2 根
+// max = (Input - 4 - 2) / 2
 // 这步应该还有很多能简化的方法，不过应该数量级不会变
 
 // 通过上面规则推算 A 最大长度
-var max_length = parseInt((input - 4) / 2 / 2, 10);
+var max_length = parseInt((input - 4 - 2) / 2 / 2, 10);
 
 // 显示次数
 var count = 0;
@@ -18,11 +19,10 @@ var count = 0;
 // 最大长度对应的最大值，由 1 组成
 var max = 0;
 
-while (max_length >= 0) {
-    max += Math.pow(10, max_length);
+while (max_length > 0) {
+    max += Math.pow(10, max_length - 1);
     max_length--;
 }
-
 
 // TODO
 // 这个函数都是重复计算可以缓存计算结果
@@ -43,8 +43,8 @@ var len = function(no) {
 // 所以可以和 max_length 配合一下优化一下循环次数
 // 这里只是演示就先不做了
 
-for( var i = 0; i <= max; i++ ) {
-    for( var j = 0; j <= max; j++ ) {
+for( var i = 1; i <= max; i++ ) {
+    for( var j = 1; j <= max; j++ ) {
         if(len(i) + len(j) + len(i + j) === (input - 4)) {
             console.log(i + " + " + j + " = " + (i + j));
             count++;
