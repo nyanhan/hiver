3d27126da9b7a2896147133f80048be454a56e7f
hiver <hany.oy@gmail.com>
2015-05-23 19:03:38 +0800

    xxx

---
+++ reactjs/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.js$/,
+                loader: "babel"
+            },
+            {
+                test: /\.jsx$/,
+                loader: "babel"
+            }
+        ]
+    }
+};
+
